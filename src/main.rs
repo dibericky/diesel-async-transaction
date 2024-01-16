47d6a26b0a3059f3cf628e34e4f45be3841ff221
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2024-01-16 17:11:46 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,14 @@ async fn main() {
     
     let result = transaction_async(&mut conn, |cn: &mut PgConnection| {
         Box::pin(async move {
-            diesel::sql_query("SELECT name from my_table ")
-            .load::<MyTable>(cn)
+            diesel::sql_query("UPDATE my_table SET name = 'ciaoooooo' where name = 'ciao2'")
+            .execute(cn)
         })
     }).await;
-    println!("RESULT: {result:?}")
+    
+    println!("RESULT: {result:?}");
+
+    let result2 = diesel::sql_query("SELECT name from my_table ")
+            .load::<MyTable>(&mut conn).unwrap();
+    println!("RESULT2: {result2:?}");
 }

9e28f89fb5b8297581bb19922ba166d95b5c059b
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2024-01-16 16:04:45 +0100

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -3,27 +3,50 @@ use diesel::{PgConnection, result::Error, Connection, RunQueryDsl, deserialize::
 use schema::my_table;
 mod schema;
 
-pub async fn transaction_async<R, E, F, Fut>(conn: &mut PgConnection, callback: F) -> Result<R, TransactionError<E>>
-    where F : FnOnce(&mut PgConnection) -> Fut, Fut: std::future::Future<Output=Result<R, E>>, E: std::fmt::Debug {
-    AnsiTransactionManager::begin_transaction(conn)
-        .map_err(|e| {
-            TransactionError::DieselError(e)
-        })?;
+// pub async fn transaction_async<'a, R, E, F, Fut>(conn: &'a mut PgConnection, callback: F) -> Result<R, TransactionError<E>>
+//     where F : FnOnce(&'a mut PgConnection) -> Fut, Fut: std::future::Future<Output=Result<R, E>>, E: std::fmt::Debug {
+//     AnsiTransactionManager::begin_transaction(conn)
+//         .map_err(|e| {
+//             TransactionError::DieselError(e)
+//         })?;
 
-    match callback(&mut *conn).await {
-        Ok(value) => {
-            AnsiTransactionManager::commit_transaction(conn)
-                .map_err(|e| {
-                    TransactionError::DieselError(e)
-                })?;
-            Ok(value)
+//     match callback(&mut *conn).await {
+//         Ok(value) => {
+//             AnsiTransactionManager::commit_transaction(conn)
+//                 .map_err(|e| {
+//                     TransactionError::DieselError(e)
+//                 })?;
+//             Ok(value)
+//         }
+//         Err(user_error) => match AnsiTransactionManager::rollback_transaction(conn) {
+//             Ok(()) | Err(Error::BrokenTransactionManager) => Err(TransactionError::CallbackError(user_error)),
+//             Err(rollback_error) => Err(TransactionError::DieselError(rollback_error)),
+//         },
+//     }
+// }
+
+pub async fn transaction_async<F, R, E>(conn: &mut PgConnection, callback: F) -> Result<R, E>
+    where
+        F: FnOnce(&mut PgConnection) -> Result<R, E>,
+        E: From<Error>,
+    {
+        AnsiTransactionManager::begin_transaction(conn)?;
+        match callback(&mut *conn) {
+            Ok(value) => {
+                AnsiTransactionManager::commit_transaction(conn)?;
+                Ok(value)
+            }
+            Err(user_error) => match AnsiTransactionManager::rollback_transaction(conn) {
+                Ok(()) => Err(user_error),
+                Err(Error::BrokenTransactionManager) => {
+                    // In this case we are probably more interested by the
+                    // original error, which likely caused this
+                    Err(user_error)
+                }
+                Err(rollback_error) => Err(rollback_error.into()),
+            },
         }
-        Err(user_error) => match AnsiTransactionManager::rollback_transaction(conn) {
-            Ok(()) | Err(Error::BrokenTransactionManager) => Err(TransactionError::CallbackError(user_error)),
-            Err(rollback_error) => Err(TransactionError::DieselError(rollback_error)),
-        },
     }
-}
 
 #[derive(FromSqlRow, Debug, QueryableByName)]
 #[diesel(table_name = my_table)]
--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,10 @@ use diesel::{PgConnection, result::Error, Connection, RunQueryDsl, deserialize::
 async fn main() {
     let mut conn = PgConnection::establish("postgresql://postgres:postgres@localhost:5552/prova").unwrap();
 
-    let result = diesel::sql_query("SELECT name from my_table ")
-            .load::<MyTable>(&mut conn)
-            .map_err(|_| ()).unwrap();
-    // let result = transaction_async(&mut conn, |cn| async { 
-    //     diesel::sql_query("SELECT name from my_table ")
-    //         .execute(cn)
-    //         .map_err(|_| ())
-    //         // .load::<EntitiesCode>(cn)
-    // }).await;
+    
+    let result = transaction_async(&mut conn, |cn| { 
+       diesel::sql_query("SELECT name from my_table ")
+            .load::<MyTable>(cn)
+    }).await;
     println!("RESULT: {result:?}")
 }
